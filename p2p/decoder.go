36ad641f1e4f3aadfd01f8f76fa81d1ed71d5b1c
ApurvX210 <apurvsharma210@gmail.com>
2025-11-17 22:49:05 +0530

    Fixing the stream issue when handling the get file method

--- p2p/decoder.go
+++ p2p/decoder.go
@@ -18,6 +18,23 @@ func (dec GobDecoder) Decode(r io.Reader, msg *RPC) error{
 type DefaultDecoder struct{}
 
 func (dec DefaultDecoder) Decode(r io.Reader, msg *RPC) error{
+	peekBuf := make([]byte,1)
+
+	_,err := r.Read(peekBuf)
+	if err != nil{
+		return err
+	}
+
+	// Incase of a stream we are not decoding what is being sent over the network
+	// We are just setting stream true so we can handle that in our handle message
+	switch peekBuf[0] {
+	case IncomingMessage:
+		msg.Stream = false
+	case IncomingStream:
+		msg.Stream = true
+		return nil
+	}
+
 	buf := make([]byte,1028)
 	n,err := r.Read(buf)
 
--- p2p/message.go
+++ p2p/message.go
@@ -1,8 +1,13 @@
 package p2p
 
+const (
+	IncomingMessage = 0x1
+	IncomingStream = 0x2
+)
 // Message hold any aribitrary data that is being sent 
 // each transport bw node and the server
 type RPC struct{
 	From 	string
 	Payload []byte
+	Stream	bool
 }
